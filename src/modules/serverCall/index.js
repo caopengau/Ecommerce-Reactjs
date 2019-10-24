14bd9ac11c0f342a1be38c50909bab36bf19d78b
Cao Peng <caopengau@gmail.com>
2019-10-24 17:15:31 +1100

    Update index.js

--- src/modules/serverCall/index.js
+++ src/modules/serverCall/index.js
@@ -5,7 +5,7 @@ import qs from 'qs'
 import paypalConfig from '../../configs/paypalConfig'
 
 // const URL = 'https://zack-ecommerce-nodejs.herokuapp.com';
-const URL = 'https://www.studyornot.com:3000';
+const URL = 'http://www.studyornot.com:3000';
 // const URL = 'http://localhost:3000';
 
 const serverCall = (config) => {
